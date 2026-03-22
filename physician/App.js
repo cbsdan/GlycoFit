add8f6307eccddcb8af4c0b52ac1417b11515ce6
Daniel Cabasa <cabasadaniel1@gmail.com>
2026-03-23 01:52:26 +0800

    fix physician error

--- physician/App.js
+++ physician/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, ActivityIndicator, Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getApp } from '@react-native-firebase/app';
